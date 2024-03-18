280aff20444bf20de4948942c82dd3ffb74698b0
Breno Rosa <breno_c_rosa@hotmail.com>
2024-03-18 13:04:51 -0300

    Update main.js

--- IMC/assets/js/main.js
+++ IMC/assets/js/main.js
@@ -5,15 +5,15 @@ form.addEventListener('submit', function (e) {
   const inputPeso = document.querySelector('#peso');
   const inputAltura = document.querySelector('#altura');
 
-  const peso = parseFloat(inputPeso.value.replace(',', '.'));
-  const altura = parseFloat(inputAltura.value.replace(',', '.'));
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-  if (!peso || isNaN(peso)) {
+  if (!peso) {
     setResultado('Peso inválido', false);
     return;
   }
 
-  if (!altura || isNaN(altura)) {
+  if (!altura) {
     setResultado('Altura inválida', false);
     return;
   }
@@ -26,26 +26,29 @@ form.addEventListener('submit', function (e) {
   setResultado(msg, true);
 });
 
-function getImc(peso, altura) {
+function getImc (peso, altura) {
   const imc = peso / altura ** 2;
   return imc;
 }
 
-function getNivelImc(imc) {
-  if (imc < 18.5) return 'Abaixo do peso';
-  if (imc < 24.9) return 'Peso normal';
-  if (imc < 29.9) return 'Sobrepeso';
-  if (imc < 34.9) return 'Obesidade grau 1';
-  if (imc < 39.9) return 'Obesidade grau 2';
-  return 'Obesidade grau 3';
+function getNivelImc (imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
 }
 
-function criaP() {
+function criaP () {
   const p = document.createElement('p');
   return p;
 }
 
-function setResultado(msg, isValid) {
+function setResultado (msg, isValid) {
   const resultado = document.querySelector('#resultado');
   resultado.innerHTML = '';
 
@@ -57,6 +60,6 @@ function setResultado(msg, isValid) {
     p.classList.add('bad');
   }
 
-  p.textContent = msg;
+  p.innerHTML = msg;
   resultado.appendChild(p);
 }
